81ee16e0d087a16b8052e29542d25daf22f47c24
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-09-26 01:49:03 +0530

    #

--- src/Additem.js
+++ src/Additem.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { FaPlus } from 'react-icons/fa6'
+import { useRef } from 'react'
 
 const Additem = ({addItem,SetAddItems,HandelSubmit}) => {
+
+    const inputRef  = useRef()
+
   return (
     <form 
     className=' addForm' onSubmit={HandelSubmit}>
         <label htmlfor ='addItem'></label>
         <input
         autoFocus  
+        ref={inputRef}
         type='text'
         id='addItem'
         placeholder='Add Items'
@@ -18,6 +23,7 @@ const Additem = ({addItem,SetAddItems,HandelSubmit}) => {
         <button 
         type='submit'
         aria-label='Add Items'
+        onClick={() => inputRef.current.focus()}
         >
             <FaPlus/>
         </button>
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Additem from "./Additem";
 import Search  from "./Search";
 import axios from "axios";
@@ -18,47 +18,52 @@ function App() {
 const [items,Setitems] = useState(
   [
        {
-            id:1,
+            id:"1",
             checked:false,
             item:"Learningreact"
        },
        {
-            id:2,
+            id:"2",
             checked:false,
             item:"Learning MongoDb"
        },
        {
-            id:3,
+            id:"3",
             checked:false,
             item:"Placements"
        },
        {
-            id:4,
+            id:"4",
             checked:false,
             item:"Learning  Subjects"
        }
   ]
 );
-     const [search,SetSearch] = useState(' ')
 
+     const [search,SetSearch] = useState(' ')
      const [addItem,SetAddItems] = useState('')
 
-     const newItem = async (item) => {
+  
+     const newItem = (item) => {
           const id = items.length ? items[items.length - 1].id + 1 : 1;
-          const newTask = { id, item };
-        
-          try {
-            const response = await axios.post("http://localhost:6969/Create", newTask);
-            const userdata = response.data;
-            console.log(userdata, "sent successfully");
-            const newItems = [...items, { id, checked: false, item }];
-            Setitems(newItems);  
-                    
-          } catch (error) {
-            console.error("Error:", error, "Data pogala");
-          }
-        };
+          const newItems = [...items, { id, checked: false, item }];
+          Setitems(newItems);
+          localStorage.setItem("DO-TO list", JSON.stringify(newItems));
+        }
 
+
+        useEffect(() => {
+          const fetchItems = async () => {
+            try {
+              const response = await axios.get('http://localhost:6969/TaskIndex');
+              Setitems(response.data);
+            } catch (error) {
+              console.error('Error fetching data:', error);
+            }
+          };
+          fetchItems();
+        }, []);
+        
 
 
       
@@ -69,6 +74,8 @@ const HandelCheck = (id) =>{
         localStorage.setItem("To-do list",JSON.stringify(listitems))
    }
 
+
+
 const HandelDelete =  (id) =>{
   const listitems = items.filter((item) =>
        item.id !==id)
@@ -76,15 +83,33 @@ const HandelDelete =  (id) =>{
      localStorage.setItem("To-do list",JSON.stringify(listitems))
 }
 
-const HandelSubmit =  (e) =>{
+// const HandelSubmit =  (e) =>{
    
-  e.preventDefault()     
-  if(!addItem) 
-  console.log(addItem)
-  newItem(addItem)
-   SetAddItems('')          
-}
+//   e.preventDefault()     
+//   if(!addItem) 
+//   console.log(addItem)
+//   newItem(addItem)
+//    SetAddItems('')          
+// }
 
+
+
+const HandelSubmit =  async (e) =>{   e.preventDefault()     
+  if(!addItem) 
+  console.log(addItem);
+  const newTask ={
+    id:items.length+1,
+    item:addItem,
+    checked:false
+  };
+  try {
+    const res = await axios.post("http://localhost:6969/Create",newTask);
+    Setitems([...items,res.data])
+    SetAddItems('')
+  } catch (error) {
+    
+  }        
+}
 
   return (
    <div className="App">
@@ -96,7 +121,7 @@ const HandelSubmit =  (e) =>{
       />
       <Search 
       Search = {search}
-      SetSearch = {SetSearch}a
+      SetSearch = {SetSearch}
       />
       <Content
       items = {items.filter(
